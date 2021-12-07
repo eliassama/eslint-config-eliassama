720a2c9380490ab7143396da1be088f8e33ce781
eliassama <elias@elias.ink>
2021-12-07 21:19:19 +0800

    feat: 增加js、ts、vue及主规则

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'standard',
+        'plugin:import/recommended',
+        'plugin:prettier/recommended',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+}
---
+++ packages/typescript/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['../../index'],
+    overrides: [
+        {
+            files: ['*.ts'],
+            extends: ['./typescript'],
+        },
+    ],
+}
