58949498bc0dda95465361c96fa2e1c62635b918
CocytusElias <elias@elias.ink>
2021-12-09 13:31:34 +0800

    fix: 增加ts下变量是否使用的规则判定

--- packages/typescript/typescript.js
+++ packages/typescript/typescript.js
@@ -15,5 +15,7 @@ module.exports = {
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
 };
