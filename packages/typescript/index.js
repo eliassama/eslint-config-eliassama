e9b1adae1f1239d89c5cd8640470986c0bc6b8f6
CocytusElias <elias@elias.ink>
2022-01-07 10:31:44 +0800

    fix: 调整整体的结构

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,9 +1,14 @@
 module.exports = {
-  extends: ['../../index'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   overrides: [
     {
       files: ['*.ts'],
-      extends: ['./typescript'],
+      extends: ['./packages/typescript'],
       rules: {
         // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
         'no-use-before-define': 'off',
@@ -14,10 +19,9 @@ module.exports = {
     },
   ],
   settings: {
+    // https://github.com/alexgorbatchev/eslint-import-resolver-typescript#configuration
     'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+      typescript: {},
     },
   },
 };
