912f8d15b1ad0e3022f6f77c8d843186d8679056
CocytusElias <elias@elias.ink>
2022-01-07 10:03:06 +0800

    feat: 增加 ts 的部分规则

--- packages/typescript/.eslintrc.js
+++ packages/typescript/.eslintrc.js
@@ -4,6 +4,20 @@ module.exports = {
     {
       files: ['*.ts'],
       extends: ['./typescript'],
+      rules: {
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
+      },
     },
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
